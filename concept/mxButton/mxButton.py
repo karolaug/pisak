d538f652ff0395ef67e1e7abda39c572e78e2ca6
Aleksander Kijek <sasza@braintech.pl>
2014-05-15 21:31:08 +0200

    first try of MxButton from json

---
+++ concept/mxButton/mxButton.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+'''
+Does button work?
+'''
+import sys
+from pisak import switcher_app
+from gi.repository import Clutter, Mx
+
+#Mx.Style.get_default().load_from_file('button.css')
+
+class ButtonStage(Clutter.Stage):
+    '''
+    Clutter stage with single button in the center
+    '''
+    def __init__(self):
+        super().__init__()
+        self.button = Mx.Button()
+        
+        self.script = Clutter.Script()
+        self.script.load_from_file('concept/mxButton/button.json')
+
+        self.button = self.script.get_object('button')
+
+
+        self.add_child(self.button)
+        self.layout = Clutter.BinLayout()
+        self.set_layout_manager(self.layout)
+
+
+class ButtonApp(switcher_app.Application):
+    '''
+    Simple app written for test purposes
+    '''
+    def create_stage(self, argv):
+        return ButtonStage()
+        
+
+if __name__ == '__main__':
+    ButtonApp(sys.argv).main()
