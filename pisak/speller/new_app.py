514b8b086a76b0f387d7b3be11f94acfbd8d80ce
Piotr Iwaniuk <piwaniuk@braintech.pl>
2014-05-15 14:17:20 +0200

    Added loadable combined speller layout refs #184

--- pisak/speller/new_app.py
+++ pisak/speller/new_app.py
@@ -1,13 +1,16 @@
 from pisak import switcher_app
-from gi.repository import Clutter
+from gi.repository import Clutter, Mx
 import sys
+import pisak.speller.widgets
 
 
 class PisakSpellerStage(Clutter.Stage):
     def __init__(self, context):
+        super().__init__()
         self.context = context
+        Mx.Button() # workaround for GI loader
         self.script = Clutter.Script()
-        self.script.load_from_file("speller_view.json")
+        self.script.load_from_file("speller_combined.json")
         view_actor = self.script.list_objects()[0]
         self.add_child(view_actor)
 
@@ -18,7 +21,7 @@ class PisakSpellerApp(switcher_app.Application):
     """
     def create_stage(self, argv):
         stage = PisakSpellerStage(self.context)
-        stage.set_fullscreen(True)
+        #stage.set_fullscreen(True)
         return stage
 
 
