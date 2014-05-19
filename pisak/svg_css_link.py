d9d792abebd280905d7f0b0fa9baa1084c7844bd
Bartosz Jura <bjura@braintech.pl>
2014-05-19 14:41:30 +0200

    Script linking the whole folders of svg and css refs #189

--- pisak/svg_css_link.py
+++ pisak/svg_css_link.py
@@ -1,5 +1,6 @@
-def svg_css_link(svg_path):
-    css_path = svg_path[ : svg_path.index('.')] + '.css'
+import sys, os
+
+def svg_css_link(svg_path, css_path):
     css_link = '<?xml-stylesheet type="text/css" href="{}"?>\n'.format(css_path)
     svg_file = open(svg_path, 'r+')
     lines = svg_file.readlines()
@@ -7,3 +8,12 @@ def svg_css_link(svg_path):
     svg_file.seek(0)
     svg_file.write(''.join(lines))
     svg_file.close()
+
+if __name__=='__main__':
+    svg_folder = sys.argv[1]
+    css_folder = sys.argv[2]
+    for svg in os.listdir(svg_folder):
+        if 'svg' in svg:
+            svg_path = svg_folder + '/' + svg
+            css_path = css_folder + '/' + svg[ : svg.index('.')] + '.css'
+            svg_css_link(svg_path, css_path)
