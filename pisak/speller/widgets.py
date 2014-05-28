9c0c19bcb5cf8decc3635cc45b70c1b469e33157
Piotr Iwaniuk <piwaniuk@braintech.pl>
2014-05-28 11:52:32 +0200

    Added target property to speller key Added typing target in combined speller refs #223

--- pisak/speller/widgets.py
+++ pisak/speller/widgets.py
@@ -1,7 +1,7 @@
 '''
 Definitions of widgets specific to speller applet
 '''
-from gi.repository import Mx, GObject, Clutter
+from gi.repository import Mx, GObject
 import pisak.widgets
 from pisak.res import dims
 
@@ -37,6 +37,16 @@ class Button(pisak.widgets.Button):
         raise NotImplementedError()
 
 
+class Text(Mx.Label):
+    __gtype_name__ = "PisakSpellerText"
+    def __init__(self):
+        super().__init__()
+        self.set_text("Foobar")
+
+    def write(self, event, key): # i tu bez event - dogadac
+        self.text_field.insert_text(text, -1) # i tu wtedy jakby Key.key
+
+
 class Key(pisak.widgets.Button):
     __gtype_name__ = "PisakSpellerKey"
     __gproperties__ = {
@@ -48,91 +58,82 @@ class Key(pisak.widgets.Button):
             GObject.PARAM_READWRITE),
         "alt_text": (
             GObject.TYPE_STRING,
-            "alt key text",
-            "AltGr string appended to a text",
+            "altgr text",
+            "altgr string appended to a text",
             "?",
             GObject.PARAM_READWRITE),
         "cl_text": (
             GObject.TYPE_STRING,
-            "caps lock key text",
-            "CapsLockGr string appended to a text",
+            "caps text",
+            "caps string appended to a text",
             "?",
             GObject.PARAM_READWRITE),
         "special_text": (
             GObject.TYPE_STRING,
-            "special key text",
-            "SpecialGr string appended to a text",
+            "special text",
+            "special string appended to a text",
             "?",
+            GObject.PARAM_READWRITE),
+        "target": (
+            Text.__gtype__,
+            "typing target",
+            "id of text box to type text",
             GObject.PARAM_READWRITE)
     }
-    
+
     __gsignals__ = {
         "writing": (GObject.SIGNAL_RUN_FIRST, None, (int,))
     }
-    
+
     def __init__(self):
         self.text = "?"
         self.alt_text = "?"
+        self.target = None
         super().__init__()
         self.set_size(dims.MENU_BUTTON_H_PX, dims.MENU_BUTTON_H_PX)
-        self.connect("notify::text", lambda *_: self._init_label())
-    
-    def _init_label(self):
-        self.set_initial_label()
-        
-    def set_label_text(self, text):
-        self.set_label(text)
-
-    def set_initial_label(self):
-        text = self.get_property("text")
-        self.set_label_text(text)
+        self.connect("activate", self.type_text)
 
     def set_alt_label(self):
-        alt_text = self.get_property("alt_text")
-        self.set_label_text(alt_text)
+        self.set_label_text(self.alt_gr_text)
 
     def set_cl_label(self):
-        cl_text = self.get_property("cl_text")
-        self.set_label_text(cl_text)
+        self.set_label_text(self.caps_text)
 
     def set_special_label(self):
-        special_text = self.get_property("special_text")
+        special_text = self.get_property("spec_text")
         self.set_label_text(special_text)
 
-    def update_button(self):
-        raise NotImplementedError()
+    def type_text(self, source):
+        if self.target:
+            text = self.target.get_text() + self.text
+            self.target.set_text(text)
 
-    def spell(self, key, event):
-        self.emit('writing', key) # tu chyba nie trzeba tego key, coś z alt zrobić
-        
     @property
     def text(self):
         return self._text
-    
+
     @text.setter
     def text(self, value):
         self._text = str(value)
         self.set_label(self.text)
-    
+
     @property
     def alt_text(self):
         return self._alt_text
-    
+
     @alt_text.setter
     def alt_text(self, value):
         self._alt_text = str(value)
 
+    @property
+    def target(self):
+        return self._target
 
-class Text(Mx.Label):
-    __gtype_name__ = "PisakSpellerText"
-    def __init__(self):
-        super().__init__()
-        self.set_text("Foobar")
+    @target.setter
+    def target(self, value):
+        self._target = value
 
-    def write(self, event, key): # i tu bez event - dogadac
-        self.text_field.insert_text(text, -1) # i tu wtedy jakby Key.key
-        
-        
+
 class Prediction(pisak.widgets.Button):
     __gtype_name__ = "PisakSpellerPrediction"
     __gproperties__ = {
