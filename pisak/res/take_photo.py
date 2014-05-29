a1d71d6b00b4dde447fca7f0f5556cb7dcfa03b9
Aleksander Kijek <sasza@braintech.pl>
2014-05-29 18:54:23 +0200

    add take photo to photo_edit based on opencv - only for PiknikNaukowy2014

---
+++ pisak/res/take_photo.py
@@ -0,0 +1,9 @@
+import camera, cv2, time, os.path
+from pisak import res
+
+cams = camera.lookForCameras()
+cam = camera.Camera(cams['Camera_1'])
+time.sleep(0.2)
+frame = cam.frame()
+cam.close()
+cv2.imwrite(os.path.join(res.PATH, 'zdjecie.jpg'), frame)
