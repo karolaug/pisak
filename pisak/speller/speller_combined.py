10978ddab0bb30008915ad661eeed17259e9457e
Aleksander Kijek <sasza@braintech.pl>
2014-05-29 22:15:51 +0200

    add styling to speller combined

--- pisak/speller/speller_combined.py
+++ pisak/speller/speller_combined.py
@@ -1,8 +1,8 @@
-import sys
+import sys, os.path
 
 from gi.repository import Clutter, Mx
 
-from pisak import switcher_app
+from pisak import switcher_app, res
 
 import pisak.speller.widgets  # @UnusedImport
 import pisak.layout  # @UnusedImport
@@ -10,10 +10,13 @@ import pisak.layout  # @UnusedImport
 
 class PisakSpellerStage(Clutter.Stage):
     SCRIPT_PATH = "speller_combined.json"
+    STYLE_PATH = os.path.join(res.PATH, "photo_edit.css")
 
     def __init__(self, context):
         super().__init__()
         self.context = context
+        self.style = Mx.Style.get_default()
+        self.style.load_from_file(self.STYLE_PATH)
         self._load_script()
 
     def _load_script(self):
@@ -21,6 +24,9 @@ class PisakSpellerStage(Clutter.Stage):
         self.script = Clutter.Script()
         self.script.load_from_file(self.SCRIPT_PATH)
         self.view_actor = self.script.get_object("main")
+        self.text_box = self.script.get_object("text_box")
+        self.text_box.get_children()[0].set_property('line-wrap', True)
+        print(self.text_box.get_children()[0].get_property('line-wrap'))
         self.set_layout_manager(Clutter.BoxLayout())
         self.add_child(self.view_actor)
 
