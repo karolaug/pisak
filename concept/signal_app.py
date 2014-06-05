ad47852455d7a3cebeed58faa64692e9fc89a35f
Piotr Iwaniuk <piwaniuk@braintech.pl>
2014-06-05 14:03:49 +0200

    Created signals module with ClutterScript signal glue Created pisak.handlers module with generic signal handlers Adapted signal concept to new modules refs #244

--- concept/signal_app.py
+++ concept/signal_app.py
@@ -4,33 +4,7 @@ Does button work?
 import sys
 from pisak import switcher_app
 from gi.repository import Clutter, Mx, GObject
-
-
-def say_hello(*args):
-    print("Hello")
-
-
-def resolve_name(handler_name):
-    """
-    Resolve python name to python value in current namespace
-    """
-    name_parts = handler_name.split('.')
-    current = sys.modules[__name__]
-    for part in name_parts:
-        current = current.__dict__[part]
-    return current
-
-def resolve_handler(handler_name):
-    function = resolve_name(handler_name)
-    if callable(function):
-        return function
-    else:
-        raise "Specified handler is not a function"
-
-
-def python_connect(script, gobject, signal, handler, target, flags):
-    function = resolve_handler(handler)
-    gobject.connect_object(signal, function, target, flags)
+from pisak import signals
 
 
 class ButtonStage(Clutter.Stage):
@@ -42,7 +16,7 @@ class ButtonStage(Clutter.Stage):
         Mx.Style.get_default()
         script = Clutter.Script()
         script.load_from_file("signals.json")
-        script.connect_signals_full(python_connect)
+        script.connect_signals_full(signals.python_connect)
         main_actor = script.get_object("main")
         self.add_child(main_actor)
         self.layout = Clutter.BinLayout()
---
+++ pisak/handlers.py
@@ -0,0 +1,9 @@
+'''
+ClutterScript signal handler library
+'''
+
+def say_hello(*args):
+    """
+    Print standard acknowledging message
+    """
+    print("Hello World!")
