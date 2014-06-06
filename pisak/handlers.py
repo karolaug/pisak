86aaaa32ef6a1864576f37abcf2e52d0c56e96e8
Piotr Iwaniuk <piwaniuk@braintech.pl>
2014-06-06 14:21:20 +0200

    Added start group handler Added exit app handler refs #244

--- pisak/handlers.py
+++ pisak/handlers.py
@@ -7,3 +7,12 @@ def say_hello(*args):
     Print standard acknowledging message
     """
     print("Hello World!")
+
+def start_group(source, *args):
+    """
+    Start scanning group
+    """
+    source.start_cycle()
+
+def exit_app(source, *args):
+    source.get_stage().destroy()
