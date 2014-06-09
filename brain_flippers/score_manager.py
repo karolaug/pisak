cad649929198a3a78a078142838a9a958472f015
Bartosz Jura <bjura@braintech.pl>
2014-06-09 14:46:15 +0200

    Added database queries for best scores refs #276

--- brain_flippers/score_manager.py
+++ brain_flippers/score_manager.py
@@ -10,15 +10,34 @@ def get_today_date():
     return time.strftime("%Y-%m-%d")
 
 def add_record(game, name, score):
+    conn = get_db_connection()
+    cur = conn.cursor()
     today = get_today_date()
     values = (today, name, score)
+    if game == "monkey":
+        cur.execute("""CREATE TABLE IF NOT EXISTS monkey
+                    (date TEXT, name TEXT, score REAL)""")
+        cur.execute("INSERT INTO monkey VALUES (?, ?, ?)", values)
+    clean_up(conn)
+
+def get_best_today(game):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    today = get_today_date()
+    if game == "monkey":
+        cur.execute("SELECT * FROM monkey WHERE date=? ORDER BY score", (today,))
+    records = cur.fetchall()[:10]
+    clean_up(conn)
+    return records
+
+def get_best_ever(game):
     conn = get_db_connection()
     cur = conn.cursor()
     if game == "monkey":
-        cur.execute("""CREATE TABLE IF NOT EXISTS monkey
-                    (date TEXT, name TEXT, points REAL)""")
-        cur.execute("INSERT INTO monkey VALUES (?, ?, ?)", values)
+        cur.execute("SELECT * FROM monkey ORDER BY score")
+    records = cur.fetchall()[:10]
     clean_up(conn)
+    return records
 
 def clean_up(conn):
     conn.commit()
