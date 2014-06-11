674472737ef5a0848244af035270ea06b05581cd
Piotr Iwaniuk <piwaniuk@braintech.pl>
2014-06-11 17:25:43 +0200

    Add dictionary with app views refs #295

--- brain_flippers/app.py
+++ brain_flippers/app.py
@@ -7,22 +7,39 @@ from brain_flippers import score_manager
 
 import pisak.widgets  # @UnusedImport
 import pisak.layout  # @UnusedImport
+import os.path
+
+VIEW_PATHS = {
+  "high_scores": "best_result_screen.json",
+  "enter_score": "user_result_screen.json",
+  "main_menu": None,
+  "game_screen": None
+}
 
 
 class BrainFlipperStage(Clutter.Stage):
-    SCRIPT_PATH = "brain_flippers/user_result_screen.json"
+    INITIAL_VIEW = "enter_score"
 
     def __init__(self, context):
         super().__init__()
+        self._init_views()
         self.context = context
         self._load_script()
 
+    def _init_views(self):
+        current_path = os.path.split(__file__)[0]
+        self.views = {}
+        for name, path in VIEW_PATHS.items():
+            if path is None:
+                continue
+            view_script = Clutter.Script()
+            view_script.load_from_file(os.path.join(current_path, path))
+            self.views[name] = view_script
+
     def _load_script(self):
-        Mx.Button() # workaround for GI loader
-        self.script = Clutter.Script()
-        self.script.load_from_file(self.SCRIPT_PATH)
+        self.script = self.views[self.INITIAL_VIEW]
         self.view_actor = self.script.get_object("main")
-        self.set_layout_manager(Clutter.BoxLayout())
+        self.set_layout_manager(Clutter.BinLayout())
         self.add_child(self.view_actor)
 
 
