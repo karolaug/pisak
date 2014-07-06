05e2ef1fabd4edac36b1515ed146c5a7458f9783
Sasza Kijek <sasza@braintech.pl>
2014-07-06 20:26:25 +0200

    video played with image refs #359

--- concept/gst_app.py
+++ concept/gst_app.py
@@ -2,14 +2,13 @@ import sys
 from pisak import switcher_app
 from gi.repository import Clutter, GObject, Mx, Gst, ClutterGst
 
-
 class ButtonStage(Clutter.Stage):
     '''
     Clutter stage with single button in the center
     '''
     def __init__(self):
-        ClutterGst.init()
         super().__init__()
+        ClutterGst.init()
         self.button = Mx.Button()
         self.button.set_label("Don't click")
         self.add_child(self.button)
@@ -17,16 +16,15 @@ class ButtonStage(Clutter.Stage):
         self.video_texture = ClutterGst.VideoTexture(**{"disable-slicing": True})
         self.add_child(self.video_texture)
         
-        descriptor = "playbin uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm"
-        self.pipeline = Gst.parse_launch(descriptor)
+        try:
+            filename = sys.argv[1]
+        except IndexError:
+            filename = input('Please provide the path to the video file you want to be played:\n')
+        self.video_texture.set_filename(filename)
+        self.video_texture.set_playing(True)
         
-        self.clutter_sink = Gst.ElementFactory.make("autocluttersink")
-        self.clutter_sink.set_property("texture", self.video_texture)
-        self.pipeline.set_property("video-sink", self.clutter_sink)
-        
-        self.pipeline.set_state(Gst.State.PLAYING)
-        
-        self.layout = Clutter.BinLayout()
+        self.layout = Clutter.BoxLayout()
+        self.layout.set_orientation(Clutter.Orientation.VERTICAL)
         self.set_layout_manager(self.layout)
 
 
