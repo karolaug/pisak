1a0d54582528ae5a03d30494f0d6929fccca266f
Piotr Iwaniuk <piwaniuk@braintech.pl>
2014-07-28 17:19:40 +0200

    Fixed paths in speller launcher Improved usage message Refactore speller launcher to remove globals abuse refs #447

--- pisak/speller/new_app.py
+++ pisak/speller/new_app.py
@@ -1,9 +1,9 @@
 import sys
+import os.path
 
 from gi.repository import Clutter, Mx
 
 from pisak import switcher_app, signals
-from pisak.speller import widgets
 
 import pisak.layout  # @UnusedImport
 import pisak.scanning  # @UnusedImport
@@ -12,18 +12,33 @@ import pisak.handlers  # @UnusedImport
 import pisak.speller.handlers  # @UnusedImport
 
 
+_PATH = os.path.abspath(os.path.split(__file__)[0])
+
+
+def _local_get(relative):
+    return os.path.join(_PATH, relative)
+
+
+SCRIPT_PATHS = {
+    "row": _local_get("speller_row.json"),
+    "column": _local_get("speller_column.json"),
+    "combined": _local_get("speller_combined.json")
+}
+
+
 class PisakSpellerStage(Clutter.Stage):
-    STYLESHEET_PATH = "speller_stylesheet.css"
+    STYLESHEET_PATH = _local_get("speller_stylesheet.css")
     
-    def __init__(self, context):
+    def __init__(self, context, script_path):
         super().__init__()
         self.context = context
+        self.script_path = script_path
         self._load_script()
-        #self._load_stylesheet()
+        self._load_stylesheet()
 
     def _load_script(self):
         self.script = Clutter.Script()
-        self.script.load_from_file(SCRIPT_PATH)
+        self.script.load_from_file(self.script_path)
         self.script.connect_signals_full(signals.connect_registered)
         self.view_actor = self.script.get_object("main")
         self.set_layout_manager(Clutter.BinLayout())
@@ -38,26 +53,25 @@ class PisakSpellerApp(switcher_app.Application):
     Pisak speller app with pisak speller stage.
     """
     def create_stage(self, argv):
-        stage = PisakSpellerStage(self.context)
+        script_path = SCRIPT_PATHS[argv[1]]
+        stage = PisakSpellerStage(self.context, script_path)
         stage.set_size(1366, 768)
         stage.set_fullscreen(True)
         return stage
-    
+
+def usage():
+    print("Usage: new_app.py VARIANT")
+    print(
+        "Where variant is on of:\n"
+        "  row - speller with row layout\n"
+        "  column - speller with column layout\n"
+        "  combined - speller with combined layout")
+
 
 if __name__ == "__main__":
-    def usage():
-        print("Parameters:\n"
-              " row - speller with row layout\n"
-              " column - speller with column layout\n"
-              " combined - speller with combined layout"
-              )
-    try:
-        SCRIPT_PATH = {
-            "row": "speller_row.json",
-            "column": "speller_column.json",
-            "combined": "speller_combined.json"
-            }[sys.argv[1]]
-        PisakSpellerApp(sys.argv).main()
-    except IndexError:
-        print("No parameters passed.")
+    if len(sys.argv) != 2:
         usage()
+    elif sys.argv[1] not in SCRIPT_PATHS:
+        usage()
+    else:
+        PisakSpellerApp(sys.argv).main()
