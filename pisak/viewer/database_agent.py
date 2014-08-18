9407f6fbd293f4289c9fda8a6cb92290741a05f1
Bartosz Jura <bjura@braintech.pl>
2014-08-18 12:10:12 +0200

    Func for inserting many photos refs #542

--- pisak/viewer/database_agent.py
+++ pisak/viewer/database_agent.py
@@ -21,46 +21,46 @@ _CREATE_FAVOURITE_PHOTOS = "CREATE TABLE IF NOT EXISTS favourite_photos ( \
 
 def get_categories():
     db = DatabaseConnector()
-    db.execute_query(_CREATE_PHOTOS)
+    db.execute(_CREATE_PHOTOS)
     query = "SELECT DISTINCT category FROM photos"
-    categories = db.execute_query(query)
+    categories = db.execute(query)
     db.close_connection()
     return categories
 
 def get_photos(category):
     db = DatabaseConnector()
-    db.execute_query(_CREATE_PHOTOS)
+    db.execute(_CREATE_PHOTOS)
     query = "SELECT * FROM photos WHERE category=" + category + " ORDER BY created_on ASC, added_on ASC"
-    photos = db.execute_query(query)
+    photos = db.execute(query)
     db.close_connection()
     return photos
 
 def get_favourite_photos():
     db = DatabaseConnector()
-    db.execute_query(_CREATE_FAVOURITE_PHOTOS)
+    db.execute(_CREATE_FAVOURITE_PHOTOS)
     query = "SELECT * FROM favourite_photos ORDER BY id DESC, created_on ASC, added_on ASC"
-    favourite_photos = db.execute_query(query)
+    favourite_photos = db.execute(query)
     db.close_connection()
     return favourite_photos
 
 def add_to_favourite_photos(path):
     db = DatabaseConnector()
-    db.execute_query(_CREATE_FAVOURITE_PHOTOS)
+    db.execute(_CREATE_FAVOURITE_PHOTOS)
     if is_in_favourite_photos(path):
         db.close_connection()
         return False
     else:
-        db.execute_query(_CREATE_PHOTOS)
+        db.execute(_CREATE_PHOTOS)
         query = "INSERT INTO favourite_photos (photos_id, path, category, created_on, added_on) \
                                                 SELECT * FROM photos WHERE path=" + path
-        db.execute_query(query)
+        db.execute(query)
         db.commit()
         db.close_connection()
         return True
 
 def is_in_favourite_photos(path):
     query = "SELECT * FROM favourite_photos WHERE path=" + path
-    favourite_photos = db.execute_query(query)
+    favourite_photos = db.execute(query)
     db.close_connection()
     if favourite_photos[0]:
         return True
@@ -71,18 +71,29 @@ def insert_photo(path, category):
     db = DatabaseConnector()
     created_on = Metadata(path).get_date_time()
     added_on = db.generate_timestamp()
-    db.execute_query(_CREATE_PHOTOS)
+    db.execute(_CREATE_PHOTOS)
     query = "INSERT INTO photos (path, category, created_on, added_on) VALUES (?, ?, ?, ?)"
     values = (path, category, created_on, added_on,)
-    db.execute_query(query, values)
+    db.execute(query, values)
+    db.commit()
+    db.close_connection()
+
+def insert_many_photos(photos_list):
+    db = DatabaseConnector()
+    added_on = db.generate_timestamp()
+    for photo in photos_list:
+        photo.append(Metadata(photo[0]).get_date_time())  # photo path as the firt item
+        photo.append(added_on)
+    query = "INSERT INTO photos (path, category, created_on, added_on) VALUES (?, ?, ?, ?)"
+    db.executemany(query, photos_list)
     db.commit()
     db.close_connection()
 
 def remove_from_favourite_photos(path):
     db = DatabaseConnector()
-    db.execute_query(_CREATE_FAVOURITE_PHOTOS)
+    db.execute(_CREATE_FAVOURITE_PHOTOS)
     query = "DELETE FROM favourite_photos WHERE path=" + path
-    db.execute_query(query)
+    db.execute(query)
     db.commit()
     db.close_connection()
 
