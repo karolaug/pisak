206564ea3bb7392784be8abd10e5f375c2d57dd9
Bartosz Jura <bjura@braintech.pl>
2014-08-18 23:16:19 +0200

    Changeg variables and printing in testing module refs #542

--- concept/viewer_database.py
+++ concept/viewer_database.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
         print("Kategorie: ", ", ".join([i["category"] for i in categories]))
         photos = database_agent.get_photos(categories[-1]["category"])
         print("Zdjęcia w kategorii " + categories[-1]["category"] + ": ")
-        for i in photos:
-            print(", ".join([str(j) for j in i]))
+        for photo in photos:
+            print(", ".join([str(list(photo.keys())[idx]) + ": " + str(j) for idx, j in enumerate(photo)]))
         database_agent.add_to_favourite_photos(photos[0]["path"], photos[0]["category"])
         print("Ulubione: ")
         for fav in database_agent.get_favourite_photos():
-            print(", ".join([str(list(fav.keys())[i]) + ": " + str(j) for i,j in enumerate(fav)]))
+            print(", ".join([str(list(fav.keys())[idx]) + ": " + str(j) for idx,j in enumerate(fav)]))
     else:
         print("No photos at or below " + path)
     
