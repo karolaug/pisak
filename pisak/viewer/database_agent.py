09857a586cf5f1d4fb5998980746dd777230310d
Bartosz Jura <bjura@braintech.pl>
2014-08-18 14:48:16 +0200

    Path and category as primary keys Insert or ignore new records Create tables if not exists in some funcs refs #542

--- pisak/viewer/database_agent.py
+++ pisak/viewer/database_agent.py
@@ -7,16 +7,15 @@ from pisak.database_manager import DatabaseConnector
 
 
 _CREATE_PHOTOS = "CREATE TABLE IF NOT EXISTS photos ( \
-                                    id INTEGER PRIMARY KEY, \
                                     path TEXT, \
                                     category TEXT, \
                                     created_on TIMESTAMP, \
-                                    added_on TIMESTAMP)"
+                                    added_on TIMESTAMP, \
+                                    PRIMARY KEY (path, category))"
 
 _CREATE_FAVOURITE_PHOTOS = "CREATE TABLE IF NOT EXISTS favourite_photos ( \
                                             id INTEGER PRIMARY KEY, \
-                                            photos_id INTEGER, \
-                                            path TEXT, \
+                                            path TEXT UNIQUE, \
                                             category TEXT, \
                                             created_on TIMESTAMP, \
                                             added_on TIMESTAMP)"
@@ -53,8 +52,8 @@ def add_to_favourite_photos(path):
         db = DatabaseConnector()
         db.execute(_CREATE_FAVOURITE_PHOTOS)
         db.execute(_CREATE_PHOTOS)
-        query = "INSERT INTO favourite_photos (photos_id, path, category, created_on, added_on) \
-                                                SELECT * FROM photos WHERE path='" + path + "' LIMIT 1"
+        query = "INSERT INTO favourite_photos (path, category, created_on, added_on) \
+                                                SELECT * FROM photos WHERE path='" + path + "'"
         db.execute(query)
         db.commit()
         db.close_connection()
@@ -73,14 +72,14 @@ def is_in_favourite_photos(path):
 
 def insert_photo(path, category):
     db = DatabaseConnector()
+    db.execute(_CREATE_PHOTOS)
     meta = Metadata(path)
     if meta.has_exif():
         created_on = meta.get_date_time()
     else:
         created_on = datetime.fromtimestamp(os.path.getctime(path))
     added_on = db.generate_timestamp()
-    db.execute(_CREATE_PHOTOS)
-    query = "INSERT INTO photos (path, category, created_on, added_on) VALUES (?, ?, ?, ?)"
+    query = "INSERT OR IGNORE INTO photos (path, category, created_on, added_on) VALUES (?, ?, ?, ?)"
     values = (path, category, created_on, added_on,)
     db.execute(query, values)
     db.commit()
@@ -88,6 +87,7 @@ def insert_photo(path, category):
 
 def insert_many_photos(photos_list):
     db = DatabaseConnector()
+    db.execute(_CREATE_PHOTOS)
     added_on = db.generate_timestamp()
     for photo in photos_list:
         meta = Metadata(photo[0])  # photo path as the first item
@@ -96,7 +96,7 @@ def insert_many_photos(photos_list):
         else:
             photo.append(datetime.fromtimestamp(os.path.getctime(photo[0])))
         photo.append(added_on)
-    query = "INSERT INTO photos (path, category, created_on, added_on) VALUES (?, ?, ?, ?)"
+    query = "INSERT OR IGNORE INTO photos (path, category, created_on, added_on) VALUES (?, ?, ?, ?)"
     db.executemany(query, photos_list)
     db.commit()
     db.close_connection()
