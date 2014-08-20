f6e5584474f4023715e7ae4d3311e12af5678302
Bartosz Jura <bjura@braintech.pl>
2014-08-20 16:16:44 +0200

    Funcs for handling fav music and inserting many Improved searching tags Added schema for favourite music refs #542

--- concept/database.py
+++ concept/database.py
@@ -11,10 +11,13 @@ _CREATE_MUSIC = "CREATE TABLE IF NOT EXISTS music( \
                                         album TEXT, \
                                         track_number INTEGER, \
                                         title TEXT NOT NULL, \
-                                        path TEXT NOT NULL, \
-                                        category TEXT NOT NULL, \
-                                        added_on TIMESTAMP, \
-                                        PRIMARY KEYS (path, category))"
+                                        path TEXT PRIMARY KEY NOT NULL, \
+                                        directory TEXT, \
+                                        added_on TIMESTAMP)"
+
+_CREATE_FAVOURITE_MUSIC = "CREATE TABLE IF NOT EXISTS favourite_music( \
+                                                    id INTEGER PRIMARY KEY, \
+                                                    path TEXT UNIQUE REFERENCES path(music))"
 
 _COLUMN_TAG_MAP = {
     "year": "DATE", "genre": "GENRE", "artist": "ARTIST",
@@ -29,8 +32,7 @@ def get_metadata(path):
         print("Could not read file: {}".format(path))
         return False
     metadata = dict()
-    for col in _COLUMN_TAG_MAP.keys():
-        tag = _COLUMN_TAG_MAP[col]
+    for col, tag in _COLUMN_TAG_MAP.items():
         if tag in file_tags.keys():
             metadata[col] = file_tags[tag][0]
         else:
@@ -38,15 +40,64 @@ def get_metadata(path):
     if not metadata["title"]:
         metadata["title"] = os.path.splitext(os.path.split(path)[-1])[0]
     return list(metadata.values)
-  
-def add_track(path, category):
+
+def remove_from_favourite_music(path):
+    db = DatabaseConnector()
+    db.execute(_CREATE_FAVOURITE_MUSIC)
+    query = "DELETE FROM favourite_music WHERE path='" + path + "'"
+    db.execute(query)
+    db.commit()
+    db.close_connection()
+
+def is_in_favourite_music(path):
+    db = DatabaseConnector()
+    db.execute(_CREATE_FAVOURITE_MUSIC)
+    query = "SELECT * FROM favourite_music WHERE path='" + path + "'"
+    favourite_music = db.execute(query)
+    db.close_connection()
+    if favourite_music:
+        return True
+    else:
+        return False
+
+def insert_to_favourite_music(path):
+    if is_in_favourite_music(path):
+        return False
+    db.DatabaseConnector()
+    db.execute(_CREATE_FAVOURITE_MUSIC)
+    query = "INSERT OR IGNORE INTO favourite_music (path) VALUES (?)"
+    values = (path,)
+    db.execute(query, values)
+    db.commit()
+    db.close_connection()
+    return True
+
+def insert_many_tracks(tracks_list):
+    added_on = db.generate_timestamp()
+    for idx, track in enumerate(tracks_list):
+        values = get_metadata(track[0])  # path as the first item
+        if not values:
+            continue
+        db.DatabaseConnector()
+        db.execute(_CREATE_MUSIC)
+        values.append(track[0])
+        values.append(track[1])
+        values.append(added_on)
+        tracks_list[idx] = values
+    query = "INSERT OR IGNORE INTO music VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    db.executemany(query, tracks_list)
+    db.commit()
+    db.close_connection()
+    return True
+
+def insert_track(path, directory):
     values = get_metadata(path)
     if not values:
         return False
     db.DatabaseConnector()
     db.execute(_CREATE_MUSIC)
     values.append(path)
-    values.append(category)
+    values.append(directory)
     values.append(db.generate_timestamp())
     query = "INSERT OR IGNORE INTO music VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
     db.execute(query, values)
