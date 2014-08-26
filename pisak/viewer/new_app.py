6b74ec67ee11e0aad0dc2ada5d99560fb8283515
Bartosz Jura <bjura@braintech.pl>
2014-08-26 19:31:14 +0200

    Added working version of viewer launcher refs #245

--- pisak/viewer/new_app.py
+++ pisak/viewer/new_app.py
@@ -2,6 +2,9 @@ import os.path
 
 from gi.repository import Clutter, Mx
 
+from pisak import res, widgets, layout
+import pisak.viewer.widgets
+
 
 _PATH = os.path.abspath(os.path.split(__file__)[0])
 
@@ -13,11 +16,25 @@ def _local_get(relative):
 VIEWS = {
     "album": {
         "path": _local_get("album.json")
-    }
+    },
     "library": {
         "path": _local_get("library.json")
-    }
+    },
     "photo": {
-        "path": _loacl_get("photo.json")
+        "path": _local_get("photo.json")
     }
 }
+
+
+if __name__ == "__main__":
+    Clutter.init([])
+    stage = Clutter.Stage()
+    Mx.Style.get_default().load_from_file(os.path.join(res.PATH, "css/viewer.css"))
+    stage.set_fullscreen(True)
+    script = Clutter.Script()
+    script.load_from_file(VIEWS["library"]["path"])
+    main = script.get_object("main")
+    stage.add_child(main)
+    stage.connect("destroy", lambda _: Clutter.main_quit())
+    stage.show_all()
+    Clutter.main()
