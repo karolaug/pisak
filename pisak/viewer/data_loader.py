e177c28a46a8233128675fd1c88ed239dbc4cbdf
Bartosz Jura <bjura@braintech.pl>
2014-09-04 18:12:12 +0200

    Improved db agent and data loader due to code revs refs #620 #623

--- pisak/viewer/data_loader.py
+++ pisak/viewer/data_loader.py
@@ -1,3 +1,7 @@
+"""
+Module for loading informations about photos stored in the file system
+and passing them to photos specific database.
+"""
 import os.path
 from datetime import datetime, timedelta
 from time import gmtime
@@ -11,26 +15,19 @@ from pisak.viewer import database_agent
 EPOCH = gmtime(0)
 
 
-_LAST_PHOTO_ADDED_ON = database_agent.get_last_photo_insertion_time()
-if _LAST_PHOTO_ADDED_ON:
-    PHOTO_REF_TIME = (_LAST_PHOTO_ADDED_ON - \
-                      datetime(EPOCH.tm_year, EPOCH.tm_mon,
+def _datetime_to_float(dttime):
+    return (dttime - datetime(EPOCH.tm_year, EPOCH.tm_mon,
                             EPOCH.tm_mday, EPOCH.tm_hour,
                             EPOCH.tm_min, EPOCH.tm_sec)) / \
                             timedelta(seconds=1)
-else:
-    PHOTO_REF_TIME = 0
 
 
-_LAST_ALBUM_ADDED_ON = database_agent.get_last_album_insertion_time()
-if _LAST_ALBUM_ADDED_ON:
-    ALBUM_REF_TIME = (_LAST_ALBUM_ADDED_ON - \
-                      datetime(EPOCH.tm_year, EPOCH.tm_mon,
-                            EPOCH.tm_mday, EPOCH.tm_hour,
-                            EPOCH.tm_min, EPOCH.tm_sec)) / \
-                            timedelta(seconds=1)
-else:
-    ALBUM_REF_TIME = 0
+PHOTO_REF_TIME = _datetime_to_float(
+    database_agent.get_last_photo_insertion_time())
+
+
+ALBUM_REF_TIME = _datetime_to_float(
+    database_agent.get_last_album_insertion_time())
 
 
 REF_TIME = database_agent.get_db_last_modification_time()
@@ -40,6 +37,13 @@ LIBRARY_DIR = xdg.get_dir("pictures")
 
 
 EXTENSIONS = (".png", ".jpg", ".jpeg", ".tiff", ".gif", ".raw", ".bmp")
+
+
+def load_new():
+    if database_agent.is_db_empty():
+        _load_new_since()
+    else:
+        _load_new_since(REF_TIME)
 
 
 def _get_photo_creation_time(photo_path):
@@ -56,30 +60,15 @@ def _get_photo_creation_time(photo_path):
     return created_on
 
 
-def _load_all():
+def _load_new_since(ref_time=0):
     path_generator = os.walk(LIBRARY_DIR)
-    for current, subdirs, files in path_generator:
-        database_agent.insert_album(current)
-        new_photos = []
-        for photo_path in [os.path.join(current, name) for name in files]:
-            if os.path.splitext(photo_path)[-1].lower() in EXTENSIONS:
-                created_on = _get_photo_creation_time(photo_path)
-                new_photos += [[photo_path, created_on]]
-        database_agent.insert_many_photos_to_album(new_photos, current)
-
-
-def load_new():
-    if database_agent.is_db_empty():
-        _load_all()
-        return
-    path_generator = os.walk(LIBRARY_DIR)
-    for current, subdirs, files in path_generator:
-        if os.path.getmtime(current) > REF_TIME:
+    for current, _subdirs, files in path_generator:
+        if os.path.getmtime(current) > ref_time:
             database_agent.insert_album(current)
             new_photos = []
             for photo_path in [os.path.join(current, name) for name in files]:
-                if os.path.getctime(photo_path) > REF_TIME:
+                if os.path.getctime(photo_path) > ref_time:
                     if os.path.splitext(photo_path)[-1].lower() in EXTENSIONS:
                         created_on = _get_photo_creation_time(photo_path)
-                        new_photos += [[photo_path, created_on]]
+                        new_photos.append([photo_path, created_on])
             database_agent.insert_many_photos_to_album(new_photos, current)
--- pisak/viewer/database_agent.py
+++ pisak/viewer/database_agent.py
@@ -1,5 +1,7 @@
+"""
+Module for managing photos specific database.
+"""
 import os
-from datetime import datetime
 from contextlib import contextmanager
 
 from sqlalchemy import Column, DateTime, String, ForeignKey, Integer, Boolean, \
@@ -186,15 +188,13 @@ def insert_many_photos(photos_list):
             album = sess.query(Album).filter(Album.name==photo[2]).first()
             new_photo = Photo(path=photo[0], created_on=photo[1])
             album.photos.add(new_photo)
-            photos_list[idx] = new_photo
-        sess.add_all(photos_list)
+            sess.add(new_photo)
 
 
 def insert_many_photos_to_album(photos_list, album_name):
-  with _establish_session() as sess:
+    with _establish_session() as sess:
         album = sess.query(Album).filter(Album.name==album_name).first()
         for idx, photo in enumerate(photos_list):
             new_photo = Photo(path=photo[0], created_on=photo[1])
             album.photos.add(new_photo)
-            photos_list[idx] = new_photo
-        sess.add_all(photos_list)
+            sess.add(new_photo)
