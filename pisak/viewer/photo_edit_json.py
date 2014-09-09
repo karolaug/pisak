719ccc64648ec07d00c37fbf25140511aedabdbf
Bartosz Jura <bjura@braintech.pl>
2014-09-09 16:46:51 +0200

    Added module for launching head json with head cursor refs #648

---
+++ pisak/viewer/photo_edit_json.py
@@ -0,0 +1,28 @@
+import os
+
+from pisak import launcher
+from pisak import cursor
+
+
+def prepare_head_editing_view(stage, script, data):
+    sprite = cursor.Group()
+    main = script.get_object("main")
+    sprite.add_child(main)
+    stage.add_child(sprite)
+    
+
+def fix_path(path):
+    return os.path.join(os.path.split(__file__)[0], path)
+
+
+VIEWER_APP = {
+    "views": {
+        "head": (fix_path("head_tracking.json"), prepare_head_editing_view)
+    },
+    "initial-view": "head",
+    "initial-data": None
+}
+
+
+if __name__ == "__main__":
+    launcher.run(VIEWER_APP)
