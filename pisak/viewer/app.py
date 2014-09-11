ad4f41cba75a40b5249304be2c3a61272bfdf1c9
Bartosz Jura <bjura@braintech.pl>
2014-09-11 21:48:52 +0200

    Connect only declared buttons in app Made some cleaning in app module Made buttons labels more consistent refs #698

--- pisak/viewer/app.py
+++ pisak/viewer/app.py
@@ -9,62 +9,48 @@ from pisak import launcher
 
 def button_to_stage(stage, script, button_name, stage_to_load, data=None):
     button = script.get_object(button_name)
-    button.connect("clicked", lambda *_: stage.load_view(stage_to_load, data))
+    if button is not None:
+        button.connect("clicked", lambda *_: stage.load_view(stage_to_load,
+                                                             data))
 
 
 def prepare_photo_view(stage, script, data):
-
     slideshow = script.get_object("slideshow_widget")
     button_to_stage(stage, script, "button_edition", 
-                    "photo_editing", {"slideshow": slideshow, "album": data["album"]})
-    button_to_stage(stage, script, "button_album", "album", {"album_name": data["album"]})
-
+                    "photo_editing", {"slideshow": slideshow,
+                                      "album": data["album"]})
+    button_to_stage(stage, script, "button_album", "album",
+                    {"album_name": data["album"]})
     button_to_stage(stage, script, "button_library", "library")
-
-    # button_to_stage(stage, script, "button_start", "start") -> start panel
     data_source = script.get_object("photo_data_source")
-    data_source.album = data["album"]  # data["album_name"]
-    slideshow.show_initial_slide(data_source.data.index(data["photo"]))  # data["index"]
-
+    data_source.album = data["album"]
+    slideshow.show_initial_slide(data_source.data.index(data["photo"]))
 
 
 def prepare_album_view(stage, script, data):
-
     button_to_stage(stage, script, "button_library", "library")
-
     library = script.get_object("library_data")
-    # for index, photo in enumerate(library.tiles):
-        # photo.connect("clicked", lambda *_: stage.load_view(
-                                                    #"photo",
-                                                    #{"index": index,
-		                              	            #"album": data["album_name"]}))
+    library.tiles_handler = lambda tile, photo, album: stage.load_view("photo",
+                                            {"photo": photo, "album": album})
+    library.album = data["album_name"]
+    # start panel:
+    # button_to_stage(stage, script, "button_start", "start_panel")
 
-    library.tiles_handler = lambda tile, photo, album: stage.load_view("photo", {"photo": photo, "album": album})
-    album = script.get_object("library_data")
-    album.album = data["album_name"]  # data["album_name"]  # also through set property should page the new album
-    # also through set property should page the new album
-
-    button_to_stage(stage, script, "button_start", "photo") # -> start panel
 
 def prepare_library_view(stage, script, data):
-
-    #button_to_stage(stage, script, "button_library", "library")
-
-    library = script.get_object("library_data")
-    library.tiles_handler = lambda tile, album: stage.load_view("album", {"album_name": album})
-
-
-    button_to_stage(stage, script, "button_start", "photo") # -> start panel
+    library_data = script.get_object("library_data")
+    library_data.tiles_handler = lambda tile, album: stage.load_view(
+        "album", {"album_name": album})
+    # start panel:
+    # button_to_stage(stage, script, "button_start", "start_panel")
 
 
 def prepare_photo_editing_view(stage, script, data):
     photo = script.get_object("slide")
     photo.photo_path = data["slideshow"].slide.photo_path
-
     button = script.get_object("button_photo")
-    button.connect("clicked", lambda *_: stage.load_view("photo", {"photo": photo.photo_path, "album": data["album"]}))
-
-    # button_to_stage(stage, script, "button_start", "start") -> start panel
+    button.connect("clicked", lambda *_: stage.load_view("photo",
+                        {"photo": photo.photo_path, "album": data["album"]}))
 
 
 def fix_path(path):
