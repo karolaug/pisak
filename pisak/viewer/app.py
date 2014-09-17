e75626160231acfbb5912748935fb1329a8b6b53
Bartosz Jura <bjura@braintech.pl>
2014-09-17 21:49:32 +0200

    Added missing references in viewer app refs #688

--- pisak/viewer/app.py
+++ pisak/viewer/app.py
@@ -17,7 +17,7 @@ def button_to_stage(stage, script, button_name, stage_to_load, data=None):
 def prepare_photo_view(stage, script, data):
     slideshow = script.get_object("slideshow_widget")
     button_to_stage(stage, script, "button_edition", 
-                    "photo_editing", {"slideshow": slideshow,
+                    "viewer/photo_editing", {"slideshow": slideshow,
                                       "album": data["album"]})
     button_to_stage(stage, script, "button_album", "viewer/album",
                     {"album_name": data["album"]})
@@ -30,19 +30,17 @@ def prepare_photo_view(stage, script, data):
 def prepare_album_view(stage, script, data):
     button_to_stage(stage, script, "button_library", "viewer/library")
     library = script.get_object("library_data")
-    library.tiles_handler = lambda tile, photo, album: stage.load_view("photo",
-                                            {"photo": photo, "album": album})
+    library.tiles_handler = lambda tile, photo, album: stage.load_view(
+        "viewer/photo", {"photo": photo, "album": album})
     library.album = data["album_name"]
-    # start panel:
-    # button_to_stage(stage, script, "button_start", "start_panel")
+    button_to_stage(stage, script, "button_start", "main_panel/main")
 
 
 def prepare_library_view(stage, script, data):
     library_data = script.get_object("library_data")
     library_data.tiles_handler = lambda tile, album: stage.load_view(
         "viewer/album", {"album_name": album})
-    # start panel:
-    # button_to_stage(stage, script, "button_start", "start_panel")
+    button_to_stage(stage, script, "button_start", "main_panel/main")
 
 
 def prepare_photo_editing_view(stage, script, data):
@@ -50,7 +48,7 @@ def prepare_photo_editing_view(stage, script, data):
     photo.photo_path = data["slideshow"].slide.photo_path
     button_to_stage(stage, script, "button_photo", "viewer/photo",
                         {"photo": photo.photo_path, "album": data["album"]})
-    
+    button_to_stage(stage, script, "button_start", "main_panel/main")
 
 def _fix_path(path):
     return os.path.join(os.path.split(__file__)[0], path)
