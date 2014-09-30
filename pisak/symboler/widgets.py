07e01d07aacbee66665e26693fc4f28fccccf161
Bartosz Jura <bjura@braintech.pl>
2014-09-30 17:45:18 +0200

    Added symboler entry Added target property for symboler data source Connected entry method to tiles refs #815

--- pisak/symboler/widgets.py
+++ pisak/symboler/widgets.py
@@ -1,11 +1,27 @@
 """
 Module with widgets specific to symboler application.
 """
-from gi.repository import Mx, Clutter
+from gi.repository import Mx, Clutter, GObject
 
-from pisak import widgets, pager, properties
+from pisak import widgets, pager, properties, layout
 from pisak.res import colors
 from pisak.symboler import database_manager
+
+
+class Entry(layout.Box):
+    """
+    Entry window for typing symbols.
+    """
+    __gtype_name__ = "PisakSymbolerEntry"
+
+    def __init__(self):
+        super().__init__()
+        self.set_x_align(Clutter.ActorAlign.START)
+
+    def append_symbol(self, tile):
+        symbol = Mx.Image()
+        symbol.set_from_file(tile.preview_path)
+        self.insert_child_below(symbol, None)
 
 
 class TilesSource(pager.DataSource, properties.PropertyAdapter):
@@ -13,6 +29,13 @@ class TilesSource(pager.DataSource, properties.PropertyAdapter):
     Data source generating tiles with symbols.
     """
     __gtype_name__ = "PisakSymbolerTilesSource"
+    __gproperties__ = {
+        "target": (
+            Entry.__gtype__,
+            "symbol inserting target",
+            "id of entry to insert symbols",
+            GObject.PARAM_READWRITE)
+    }
     
     def __init__(self):
         super().__init__()
@@ -20,12 +43,22 @@ class TilesSource(pager.DataSource, properties.PropertyAdapter):
         self.data = database_manager.get_all_symbols()
         self.data_length = len(self.data)
 
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, value):
+        self._target = value
+
     def _generate_tiles(self, count):
         tiles = []
         for index in range(self.index, self.index + count):
             if index < len(self.data):
                 tile = widgets.PhotoTile()
                 tile.hilite_tool = widgets.Aperture()
+                tile.connect("activate", lambda source, tile:
+                             self.target.append_symbol(tile), tile)
                 tile.set_background_color(colors.LIGHT_GREY)
                 tile.ratio_width = self.tile_ratio_width
                 tile.ratio_height = self.tile_ratio_height
