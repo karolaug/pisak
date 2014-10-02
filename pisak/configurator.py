7fbf96e2151580e127298661f3dc1559a10d5ba5
Aleksander Kijek <sasza@braintech.pl>
2014-10-02 13:31:06 +0200

    setter for config_path in configurable class refs #788

--- pisak/configurator.py
+++ pisak/configurator.py
@@ -5,15 +5,22 @@ class Configurable(object):
     __gtype_name__ = False # __gtype_name__ should be overwritten by child
     __gproperties__ = {} # __gproperties__ should be overwritten by child
 
+    def __init__(self):
+        self.config_path = None
 
-    def __init__(self, filename):
-        self.filename = filename
-        self.readConfig()
+    @property
+    def config(self):
+        return self._config
 
-    def readConfig(self):
-        self.config = configobj.ConfigObj(self.filename)
+    @config.setter
+    def config(self, config_path):
+        self.config_path = config_path
+        if self.config_path:
+            self._config = configobj.ConfigObj(self.config_path)
+        else:
+            raise ValueError("No config specified.")
 
-    def applyProp(self, prop, default):
+    def getProp(self, prop, default):
         if self.__gtype_name__:
             try:
                 return self.config[self.__gtype_name__][prop]
@@ -25,12 +32,11 @@ class Configurable(object):
     def applyProps(self):
         if self.__gproperties__:
             for prop in self.__gproperties__.keys():
-                default = self.config["defaults"][self.__gtype_name__][prop]
-                setattr(self, prop, self.applyProp(prop, default))
+                default = getattr(self, prop)
+                setattr(self, prop, self.getProp(prop, default))
         else:
             raise ValueError("__gproperties__ not overwritten by child.")
-            
-    
+
     def writeConfig(self, where):
         self.config.filename = where
         self.config.write()
@@ -39,5 +45,6 @@ class Configurable(object):
 if __name__ == '__main__':
     import os.path
     from pisak import res
-    config = Configurable(os.path.join(res.get("configs"), "config.ini"))
-    print('{}: {}'.format(group, config.config[group]) for group in config.config.keys())
+    config_wrapper = Configurable()
+    config_wrapper.config = os.path.join(res.get("configs"), "config.ini")
+    print(*('{}: {}'.format(group, config_wrapper.config[group]) for group in config_wrapper.config.keys()), sep='\n')
